06ae707a62a98099bd4a08c0c8b20e9ad8ac98fe
arafatyamin <mdh631987@gmail.com>
2021-01-05 02:09:36 +0600

    this is bangla vai

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#bar1').barfiller({barColor: "#990000",duration: 1000,animateOnResize: false,tooltip: false,symbol: ""});
+    $('#bar2').barfiller({barColor: "#990000",duration: 1000,animateOnResize: false,tooltip: false,symbol: ""});
+    $('#bar3').barfiller({barColor: "#990000",duration: 1000,animateOnResize: false,tooltip: false,symbol: ""});
+    $('#bar4').barfiller({barColor: "#990000",duration: 1000,animateOnResize: false,tooltip: false,symbol: ""});
+});
